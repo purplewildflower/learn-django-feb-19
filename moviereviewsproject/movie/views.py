f49e97d76d86b2b96560a7b3a669fb95f499c520
purplewildflower <ehemilyharrison@gmail.com>
2023-02-19 15:35:53 -0700

    Made model - Movie

--- moviereviewsproject/movie/views.py
+++ moviereviewsproject/movie/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+    searchTerm = request.GET.get('searchMovie')
+    return render(request, 'home.html', {'searchTerm':searchTerm})
+
+def about(request):
+    return render(request, 'about.html')
+
+def signup(request):
+    email = request.GET.get('email')
+    return render(request, 'signup.html', {'email':email})
